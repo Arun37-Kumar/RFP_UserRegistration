e3edc64c95421316e12e47985a3c3e6d533f6164
Arun37-Kumar <arun007624@gmail.com>
2022-04-15 17:27:03 +0530

    commit

--- UserValidation/Program.cs
+++ UserValidation/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace UserValidation
 {
@@ -11,6 +12,15 @@ namespace UserValidation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem !");
+
+            //Creating Object
+            Validation validate = new Validation();
+
+            Console.WriteLine("Enter the First Name : ");
+            string firstname = Console.ReadLine();
+            bool fName = validate.FirstNameValidate(firstname);
+            validate.Result(fName);
+
             Console.ReadLine();
         }
     }
---
+++ UserValidation/Validation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserValidation
+{
+    class Validation
+    {
+        private string FIRST_NAME = "[A-Z][a-z]{3,}";
+
+        public bool FirstNameValidate(string firstName)
+        {
+            return Regex.IsMatch(firstName, FIRST_NAME);
+        }
+
+        public void Result(bool result)
+        {
+            if (result)
+                Console.WriteLine("Value is valid!");
+            else
+                Console.WriteLine("Value is invalid!");
+        }
+
+    }
+}
